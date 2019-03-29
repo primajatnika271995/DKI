9458a77b84de8077a82db3b930e41d9677e1cfff
primajatnika <jatnika_prima@yahoo.com>
2019-03-29 13:08:40 +0700

    lib/views/home: Rubber for Google Maps layer & Input layer

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -11,30 +11,55 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
   // Google Maps Controller
   Completer<GoogleMapController> _controller = new Completer();
-  static const LatLng _center = const LatLng(-6.6612506, 106.4010367);
+  static const LatLng _center = const LatLng(-6.2295712, 106.7594782);
 
   // Rubber Controller
   RubberAnimationController _rubberAnimationController;
   ScrollController _scrollController = new ScrollController();
 
+  Widget _getLowerLayer() {
+    return GoogleMap(
+      onMapCreated: _onCreateGoogleMaps,
+      initialCameraPosition: CameraPosition(target: _center, zoom: 15.0),
+    );
+  }
+
+  Widget _getUpperLayer() {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          controller: _scrollController,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(title: Text("Item $index"));
+          },
+          itemCount: 100),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new Stack(
-        children: <Widget>[
-          GoogleMap(
-            onMapCreated: _onCreateGoogleMaps,
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 8.0
-            ),
-          )
-        ],
-      ),
+      body: RubberBottomSheet(
+        scrollController: _scrollController,
+        lowerLayer: _getLowerLayer(),
+        upperLayer: _getUpperLayer(),
+        animationController: _rubberAnimationController,
+      )
     );
+  }
+
+  @override
+  void initState() {
+    _rubberAnimationController = RubberAnimationController(
+        vsync: this,
+        halfBoundValue: AnimationControllerValue(percentage: 0.5),
+        duration: Duration(milliseconds: 200)
+    );
+    super.initState();
   }
 
   void _onCreateGoogleMaps(GoogleMapController controller) {
