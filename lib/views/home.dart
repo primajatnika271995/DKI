dfa44701855307b0009d5b74516bf5e20961c6eb
primajatnika <jatnika_prima@yahoo.com>
2019-03-29 11:22:41 +0700

    lib/views/home: Add Center for Maps

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   final Widget child;
@@ -9,10 +11,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Google Maps Controller
+  Completer<GoogleMapController> _controller = new Completer();
+  static const LatLng _center = const LatLng(-6.6612506, 106.4010367);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: new Stack(
+        children: <Widget>[],
+      ),
     );
   }
 }
