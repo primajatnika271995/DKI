60bf9078999a3083a570dcadab019b892e2a740e
primajatnika <jatnika_prima@yahoo.com>
2019-03-29 11:12:18 +0700

    Initial Commit

---
+++ lib/routers.dart
@@ -0,0 +1,8 @@
+import 'package:dki_mobile_app/views/home.dart';
+import 'package:dki_mobile_app/views/login.dart';
+import 'package:flutter/material.dart';
+
+final routes = {
+  '/login': (BuildContext context) => LoginPage(),
+  '/home': (BuildContext context) => HomePage(),
+};
